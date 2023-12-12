20ad58f9bab183669970eff309ca26b32f04c840
pavlo <paihitetty444@gmail.com>
2023-12-12 15:37:22 +0100

    La version definitive

--- src/ClasseIPv4.java
+++ src/ClasseIPv4.java
@@ -3,7 +3,8 @@ public class ClasseIPv4 {
     private final String _0_a_255 = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
     private final String _0_a_126 = "([0-9]|[1-9][0-9]|1[0-1][0-9]|12[0-6])";
     private final String _128_a_191 = "(12[8-9]|1[3-8][0-9]|19[0-1])";
-    private final String apipClasseA = "(1[1-9]|[2-9][0-9]|1[0-1][0-9]|12[0-6])";
+    private final String apipClasse = "";
+    private final String adressesPrivate = "((10|172|192)[.])" + "(" + _0_a_255 + "|(1[6-9]|2[0-9])|3[0-1])|(168)[.]" + _0_a_255 + "[.]" + _0_a_255;
 
     public boolean classA(String ipAdresse) {
         return ipAdresse.matches(_0_a_126 + "[.]" + _0_a_255 + "[.]" + _0_a_255 + "[.]" + _0_a_255);
@@ -17,11 +18,11 @@ public class ClasseIPv4 {
     public boolean routeParDefaut(String ipAdresse) {
         return ipAdresse.matches("0" + "[.]" + _0_a_255 + "[.]" + _0_a_255 + "[.]" + _0_a_255);
     }
-    public boolean adressePriveeClasseA(String ipAdresse) {
-        return ipAdresse.matches("10" + "[.]" + _0_a_255 + "[.]" + _0_a_255 + "[.]" + _0_a_255);
+    public boolean adressePriveeClasse(String ipAdresse) {
+        return ipAdresse.matches(adressesPrivate);
     }
-    public boolean adresseApipaClasseA(String ipAdresse) {
-        return ipAdresse.matches(apipClasseA + "[.]" + _0_a_255 + "[.]" + _0_a_255 + "[.]" + _0_a_255) || ipAdresse.matches("[1-9]" + "[.]" + _0_a_255 + "[.]" + _0_a_255 + "[.]" + _0_a_255);
+    public boolean adresseApipaClasse(String ipAdresse) {
+       return true;
     }
     public boolean loopback(String ipAdresse) {
         return ipAdresse.matches("127" + "[.]" + _0_a_255 + "[.]" + _0_a_255 + "[.]" + _0_a_255);
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         ClasseIPv4 classeIPv4 = new ClasseIPv4();
-        String myIP = "10.255.255.255";
+        String myIP;
 
-        if (classeIPv4.classA(myIP)) {
-            System.out.println("Classe A");
-        }
-        if (classeIPv4.loopback(myIP)) {
-            System.out.println("Classe A");
-            System.out.print("Loopback");
-        }
-        if (classeIPv4.adresseApipaClasseA(myIP)) {
-            System.out.print("Apipa");
-        }
-        if (classeIPv4.adressePriveeClasseA(myIP)) {
-            System.out.print("Classe Privee");
-        }
-        if (classeIPv4.routeParDefaut(myIP)) {
-            System.out.print("Route par defaut");
-        }
-        if (classeIPv4.classB(myIP)) {
-            System.out.print("Classe B");
-        }
-        if (classeIPv4.classC(myIP)) {
-            System.out.print("Classe C");
+        System.out.print("Votre adresse IP: ");
+        myIP = In.readString();
+
+        assert myIP != null;
+
+        if (classeIPv4.adressePriveeClasse(myIP)) {
+            System.out.print("Private adresse");
         }
     }
 }
